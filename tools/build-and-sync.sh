41b158598cd970542269217e662e4951401dafaa
Anthony Martin <anthony.j.martin142@gmail.com>
2018-09-07 18:41:03 +0000

    Setup CI/CD

---
+++ tools/build-and-sync.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+APPVER="0.1.1"
+
+# Build the binaries
+docker build --rm -t aether-report-build -f Dockerfile .
+docker run --rm -i -v $PWD:/go/src/gitlab.com/anthony.j.martin/aether-report aether-report-build  make  #go build -a -v -o build/linux/amd64/aether-report -ldflags="-X main.version=${APPVER}"  cmd/aether-report/main.go
+#docker rmi aether-report-build
+
+# Build the RPM
+docker build --rm -t aether-report-rpm -f Dockerfile.RPM .
+docker run --rm -i -v $PWD:/go/src/gitlab.com/anthony.j.martin/aether-report aether-report-rpm go-bin-rpm generate -a amd64 -o aether-report-${APPVER}.rpm --version ${APPVER}
+#docker rmi aether-report-rpm
+
+#GOOS=linux go build -a -o build/linux/amd64/aether-report -ldflags="-X main.version=${APPVER}"  cmd/aether-report/main.go
+rsync -av --progress build/* ~/Dropbox/gotesting/ --exclude .hold
+
+
+go-bin-rpm generate -a amd64 -o build/linux/amd64/aether-report-$(./tools/version.sh).rpm --version $(./tools/version.sh)
